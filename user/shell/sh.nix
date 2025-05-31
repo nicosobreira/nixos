830103982e8781ff12c1b4f5a33682098566109f
nicosobreira <nicolau.sobreira@gmail.com>
2025-05-31 15:17:47 -0300

    Add fish

--- user/shell/sh.nix
+++ user/shell/sh.nix
@@ -16,13 +16,60 @@ let
     egrep = "egrep --color=auto";
     ls = "ls -h --color=auto";
     la = "ls --almost-all --dereference-command-line --color=auto --format=single-column --human-readable --size --group-directories-first --sort=version";
-    tree = "tree -a -C";
+    tree = "tree -C";
   };
 in
 {
+
+  home.sessionVariables = {
+    LESS = "-R";
+  };
+
+  home.shell.enableFishIntegration = true;
+  programs.fish = {
+    enable = true;
+    shellAliases = myAliases;
+    functions = {
+      fish_prompt = {
+        body = ''
+set -l last_status $status
+set -l normal (set_color normal)
+set -l status_color (set_color brgreen)
+set -l cwd_color (set_color brblue)
+set -l vcs_color (set_color brpurple)
+set -l prompt_status ""
+
+# Since we display the prompt on a new line allow the directory names to be longer.
+set -q fish_prompt_pwd_dir_length
+or set -lx fish_prompt_pwd_dir_length 0
+
+# Color the prompt differently when we're root
+set -l suffix '$'
+if functions -q fish_is_root_user; and fish_is_root_user
+  if set -q fish_color_cwd_root
+    set cwd_color (set_color $fish_color_cwd_root)
+  end
+  set suffix '#'
+end
+
+# Color the prompt in red on error
+if test $last_status -ne 0
+  set status_color (set_color $fish_color_error)
+  set prompt_status $status_color "[" $last_status "]" $normal
+end
+
+echo
+echo -s $cwd_color (prompt_pwd) $vcs_color (fish_vcs_prompt) $normal ' ' $prompt_status
+echo -n -s $status_color $suffix ' ' $normal
+        '';
+      };
+    };
+  };
+
   programs.bash = {
     enable = true;
     enableCompletion = true;
     shellAliases = myAliases;
   };
+
 }
