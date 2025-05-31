e1955363f31c652b99394ab0ce7a8ec01991afbf
nicosobreira <nicolau.sobreira@gmail.com>
2025-05-30 22:23:03 -0300

    sla

--- flake.nix
+++ flake.nix
@@ -3,25 +3,27 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager/master";
+    home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = { self, nixpkgs, home-manager, ... }:
   let
+    username = "acerola";
     system = "x86_64-linux";
+
     lib = nixpkgs.lib;
     pkgs = nixpkgs.legacyPackages.${system};
   in {
     nixosConfigurations = {
-      inherit system;
       nicolas = lib.nixosSystem {
+        inherit system;
         modules = [ ./configuration.nix ];
       };
     };
-    homeConfigurations = {
-      acerola = home-manager.lib.homeManagerConfiguration {
+    homeConfigurations.${username} = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
+        defaultPackage.${system} = home-manager.defaultPackage.${system};
         modules = [ ./home.nix ];
       };
     };
