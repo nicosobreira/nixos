3a1aa09923dbbe021bae410fbfe5b60cf4e4caa5
nicosobreira <nicolau.sobreira@gmail.com>
2025-06-07 08:19:32 -0300

    nvim: using nvim-tmux-navigator

--- user/app/nvim/lua/plugins/tmux-navigator.lua
+++ user/app/nvim/lua/plugins/tmux-navigator.lua
@@ -1,22 +1,21 @@
 return {
-	"christoomey/vim-tmux-navigator",
-	event = function()
-		if vim.fn.exists "$TMUX" == 1 then
-			return "VeryLazy"
-		end
-	end,
-	cmd = {
-		"TmuxNavigateLeft",
-		"TmuxNavigateDown",
-		"TmuxNavigateUp",
-		"TmuxNavigateRight",
-		"TmuxNavigatePrevious",
-	},
-	keys = {
-		{ "<C-h>", "<cmd><C-U>TmuxNavigateLeft<CR>" },
-		{ "<C-j>", "<cmd><C-U>TmuxNavigateDown<CR>" },
-		{ "<C-k>", "<cmd><C-U>TmuxNavigateUp<CR>" },
-		{ "<C-l>", "<cmd><C-U>TmuxNavigateRight<CR>" },
-		{ "<C-\\>", "<cmd><C-U>TmuxNavigatePrevious<CR>" },
-	},
+  "alexghergh/nvim-tmux-navigation",
+  event = function()
+    if vim.fn.exists "$TMUX" == 1 then
+      return "VeryLazy"
+    end
+  end,
+  config = function()
+    require("nvim-tmux-navigation").setup {
+      disable_when_zoomed = true, -- defaults to false
+      keybindings = {
+        left = "<C-h>",
+        down = "<C-j>",
+        up = "<C-k>",
+        right = "<C-l>",
+        last_active = "<C-\\>",
+        next = "<C-Space>",
+      }
+    }
+  end
 }
