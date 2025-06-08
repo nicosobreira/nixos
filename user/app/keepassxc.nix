ca205fd95009560f284d76ac3ad46f835c3d9756
nicosobreira <nicolau.sobreira@gmail.com>
2025-06-08 12:34:15 -0300

    nix: disable settings

--- user/app/keepassxc.nix
+++ user/app/keepassxc.nix
@@ -2,16 +2,4 @@
 
 {
   programs.keepassxc.enable = true;
-  programs.keepassxc.settings = {
-    Browser.Enabled = true;
-
-    GUI = {
-      AdvancedSettings = true;
-      ApplicationTheme = "dark";
-      CompactMode = true;
-      HidePasswords = true;
-    };
-
-    # SSHAgent.Enabled = true;
-  };
 }
