41fef98dfc20278757548544739d79eaff0fce9f
nicosobreira <nicolau.sobreira@gmail.com>
2025-06-08 16:15:00 -0300

    nix: syncthing enable

--- user/app/syncthing.nix
+++ user/app/syncthing.nix
@@ -7,11 +7,5 @@ in
 {
   services.syncthing = {
     enable = true;
-    group = "users";
-    user = userSettings.username;
-    dataDir = dataDir;
-    configDir = configDir;
-    overrideDevices = true;  # overrides any devices added or deleted through the WebUI
-    overrideFolders = true;  # overrides any folders added or deleted through the WebUI
   };
 }
