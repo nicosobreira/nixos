739aee4738afea003acf0bd802f933aa35b49e92
nicosobreira <nicolau.sobreira@gmail.com>
2025-06-08 10:50:49 -0300

    nvim: Fix treesitter

--- user/app/nvim/lua/plugins/treesitter.lua
+++ user/app/nvim/lua/plugins/treesitter.lua
@@ -1,37 +1,41 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  branch = 'main',
+  -- branch = 'master',
   lazy = false,
   build = ":TSUpdate",
   cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
   config = function()
-    require'nvim-treesitter'.install {
-      "nix",
-      "fish",
-      "bash",
-      "c",
-      "diff",
-      "html",
-      "javascript",
-      "jsdoc",
-      "json",
-      "jsonc",
-      "lua",
-      "luadoc",
-      "luap",
-      "markdown",
-      "markdown_inline",
-      "printf",
-      "python",
-      "query",
-      "regex",
-      "toml",
-      "tsx",
-      "typescript",
-      "vim",
-      "vimdoc",
-      "xml",
-      "yaml",
-    }
-  end;
+    local configs = require("nvim-treesitter.configs")
+    configs.setup({
+      ensure_installed = {
+        "nix",
+        "fish",
+        "bash",
+        "c",
+        "diff",
+        "html",
+        "javascript",
+        "jsdoc",
+        "json",
+        "jsonc",
+        "lua",
+        "luadoc",
+        "luap",
+        "markdown",
+        "markdown_inline",
+        "printf",
+        "python",
+        "query",
+        "regex",
+        "toml",
+        "tsx",
+        "typescript",
+        "vim",
+        "vimdoc",
+        "xml",
+        "yaml",
+        }
+      }
+    )
+  end
 }
