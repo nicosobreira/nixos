632e0a8802415b8bb26ce8ef15aca2d19e19105b
nicosobreira <nicolau.sobreira@gmail.com>
2025-06-08 22:05:47 -0300

    nix: not using chromium config

--- user/app/browser/chromium.nix
+++ user/app/browser/chromium.nix
@@ -2,14 +2,4 @@
 
 {
   programs.chromium.enable = true;
-  programs.chromium.extensions = [
-    # Ublock Origin Lite
-    { id = "ddkjiahejlhfcafbddmgiahcphecmpfh"; }
-
-    # KeePassXC-Browser
-    { id = "oboonakemofpalcgghocfoadofidjkkk"; }
-
-    # Enhancer for Youtube
-    { id = "ponfpcnoihfmfllpaingbgckeeldkhle"; }
-  ];
 }
