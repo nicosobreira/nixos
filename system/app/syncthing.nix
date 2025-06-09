af421de03fa6b3c83c10dfbbb0d5c588aeffe4cd
nicosobreira <nicolau.sobreira@gmail.com>
2025-06-08 22:05:55 -0300

    nix: syncthing config

--- system/app/syncthing.nix
+++ system/app/syncthing.nix
@@ -14,19 +14,21 @@ in
     user = userSettings.username;
     inherit dataDir;
     inherit configDir;
-    # overrideDevices = true;  # overrides any devices added or deleted through the WebUI
-    # overrideFolders = true;  # overrides any folders added or deleted through the WebUI
-    # settings = {
-    #   devices = {
-    #   "device1" = { id = "DEVICE-ID-GOES-HERE"; };
-    #   "device2" = { id = "DEVICE-ID-GOES-HERE"; };
-    #   };
-    #   folders = {
-    #     "Documents" = {  # Name of folder in Syncthing, also the folder ID
-    #       path = dataDir;  # Which folder to add to Syncthing
-    #       devices = [ "device1" "device2" ];  # Which devices to share the folder with
-    #     };
-    #   };
-    # };
+    guiAddress = "127.0.0.1:8384";
+    overrideDevices = true; # overrides any devices added or deleted through the WebUI
+    overrideFolders = true; # overrides any folders added or deleted through the WebUI
+    settings = {
+      devices = {
+        "nicolas" = { id = "HHZ7HL6-3CCCNLU-UHSIYC3-LZC7IXM-MKEYHVH-RREVQAA-GEAGKXD-OXU6IAA"; };
+        "phone" = { id = "BAQCHX4-A3IAOHL-ZFZCTBP-S565OWO-KYINHHN-WHKTQK3-B7YGP4W-4WPFHA6"; };
+      };
+      folders = {
+        "Documents" = {
+          # Name of folder in Syncthing, also the folder ID
+          path = dataDir; # Which folder to add to Syncthing
+          devices = [ "nicolas" "phone" ]; # Which devices to share the folder with
+        };
+      };
+    };
   };
 }
