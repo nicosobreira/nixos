d364aed5d94af9b6cb3b2fdc21826afffd9da582
nicosobreira <nicolau.sobreira@gmail.com>
2025-06-14 16:26:05 -0300

    nix: add retroarch

--- system/app/games.nix
+++ system/app/games.nix
@@ -7,18 +7,26 @@
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
     localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
   };
-  # programs.steam.gamescopeSession = true;
   programs.gamemode.enable = true;
   programs.appimage.enable = true;
   programs.appimage.binfmt = true;
 
-  # Xpad setup
   environment.systemPackages = with pkgs; [
+    # Xpad setup
     linuxKernel.packages.linux_zen.xpad-noone
 
     # Games
     pcsx2
     mame
+    (retroarch.withCores (cores: with cores; [
+      genesis-plus-gx
+      snes9x
+      beetle-psx-hw
+      desmume
+      mgba
+      parallel-n64
+      pcsx2
+    ]))
   ];
 
   nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
