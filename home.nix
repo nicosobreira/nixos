d7c2f04a3f48fc28269f51812c1d797bf75d96a6
nicosobreira <nicolau.sobreira@gmail.com>
2025-06-14 10:56:30 -0300

    nix: home.nix cleanup

--- home.nix
+++ home.nix
@@ -22,32 +22,11 @@
     ./user/wm/awesome/awesome.nix
   ];
 
-  # gtk = {
-  #   enable = true;
-  #   theme = {
-  #     name = "Adwaita-dark";
-  #     package = pkgs.gnome-themes-extra;
-  #   };
-  # };
-
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
     # # Development
     tree
-
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" "CascadiaCode" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
   ];
 
   xdg.enable = true;
@@ -75,23 +54,8 @@
     };
   };
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/acerola/etc/profile.d/hm-session-vars.sh
-  #
   home.sessionVariables = {
+    PAGER = "less -R";
     EDITOR = userSettings.editor;
     TERM = userSettings.terminal;
     BROWSER = userSettings.browser;
