314728393861d159ecde5c2510fd199a8a33f811
nicosobreira <nicolau.sobreira@gmail.com>
2025-06-16 21:05:14 -0300

    nix: games submodules

--- system/app/games.nix
+++ system/app/games.nix
@@ -1,38 +1,20 @@
-{ lib, pkgs, ... }:
+{ pkgs, ... }:
 
 {
-  programs.steam = {
-    enable = true;
-    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
-  };
+
+  imports = [
+    ./games/steam.nix
+    ./games/retro.nix
+  ];
+
   programs.gamemode.enable = true;
   programs.appimage.enable = true;
   programs.appimage.binfmt = true;
+  hardware.xone.enable = true;
 
   environment.systemPackages = with pkgs; [
     # Xpad setup
     linuxKernel.packages.linux_zen.xpad-noone
-
-    # Games
-    pcsx2
-    mame-tools
-    (retroarch.withCores (cores: with cores; [
-      genesis-plus-gx
-      snes9x
-      beetle-psx-hw
-      desmume
-      mgba
-      parallel-n64
-      pcsx2
-    ]))
   ];
 
-  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
-    "steam"
-    "steam-original"
-    "steam-unwrapped"
-    "steam-run"
-  ];
 }
---
+++ system/app/games/retro.nix
@@ -0,0 +1,17 @@
+{ pkgs, ... }:
+
+{
+  environment.systemPackages = with pkgs; [
+    # Games
+    pcsx2
+    mame-tools
+    (retroarch.withCores (cores: with cores; [
+      genesis-plus-gx
+      snes9x
+      beetle-psx-hw
+      desmume
+      mgba
+      parallel-n64
+    ]))
+  ];
+}
