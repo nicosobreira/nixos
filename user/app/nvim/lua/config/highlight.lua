4932b1542d13addd8cba8795a208a17e1817cf07
nicosobreira <nicolau.sobreira@gmail.com>
2025-07-03 21:11:52 -0300

    Remove nvim

--- user/app/nvim/lua/config/highlight.lua
+++
@@ -1,8 +0,0 @@
--- vim.opt.listchars use lite gray
-vim.cmd("highlight SpecialKey ctermfg=darkgray guifg=gray70")
-
--- Cursor line with bold
-vim.cmd("highlight CursorLineNR cterm=bold")
-
--- Spell bad habe underline and black background
-vim.cmd("highlight SpellBad cterm=underline ctermbg=Red")
