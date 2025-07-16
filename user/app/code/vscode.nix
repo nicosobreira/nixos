ad3b5661bbbad8e8accb8f04575b02471ffd7ec8
nicosobreira <nicolau.sobreira@gmail.com>
2025-07-16 19:54:54 -0300

    Simpler vscode managment

--- user/app/code/vscode.nix
+++ user/app/code/vscode.nix
@@ -1,18 +1,7 @@
 { pkgs, ... }:
 
-let
-  default = "default";
-in
 {
-  programs.vscode = {
-    enable = true;
-    programs.vscode.mutableExtensionsDir = true;
-  };
-
-  programs.vscode.profiles.${default} = {
-    extensions = with pkgs.vscode-extensions; [
-      vscodevim.vim
-      ms-vscode-remote.remote-containers
-    ];
-  };
+  home.packages = with pkgs; [
+    vscode
+  ];
 }
