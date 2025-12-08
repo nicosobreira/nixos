ea6f4155e288065fd01bee3daa2d0c1b17fbbb95
nicosobreira <nicolau.sobreira@gmail.com>
2025-12-08 13:07:40 -0300

    Lutris pcsx2

--- user/lutris.nix
+++ user/lutris.nix
@@ -4,6 +4,11 @@
   programs.lutris.enable = true;
 
   programs.lutris.runners = {
-    pcsx2.package = pkgs.pcsx2;
+    pcsx2 = {
+      package = pkgs.pcsx2;
+      settings = {
+        runner.fullscreen = true;
+      };
+    };
   };
 }
