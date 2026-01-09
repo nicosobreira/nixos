df8c48a6a39ea469261d06d2dd8b1d314263bf11
nicosobreira <nicolau.sobreira@gmail.com>
2026-01-09 16:54:26 -0300

    Using windows 95 theme

--- system/stylix.nix
+++ system/stylix.nix
@@ -11,7 +11,8 @@
       size = 20;
     };
 
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/windows-95.yaml";
+    # base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
     # base24Scheme = "${pkgs.base24-schemes}/share/themes/catppuccin-mocha.yaml";
     image = ./../wallpapers/nausicaa/1.png;
     polarity = "dark";
@@ -39,7 +40,7 @@
     };
 
     fonts.sizes = {
-      applications = 12;
+      applications = 10;
       terminal = 9;
       desktop = 12;
       popups = 12;
