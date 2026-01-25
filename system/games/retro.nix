f9256710b452c2f3ceb57ef4eaca970d32c330fd
nicosobreira <nicolau.sobreira@gmail.com>
2026-01-25 17:52:49 -0300

    Stuff

--- system/games/retro.nix
+++ system/games/retro.nix
@@ -1,6 +1,7 @@
 {pkgs, ...}: {
   environment.systemPackages = with pkgs; [
     mame-tools
+    parallel-launcher
     (retroarch.withCores (
       cores:
         with cores; [
@@ -11,7 +12,6 @@
           beetle-psx-hw
           melonds
           mgba
-          parallel-n64
         ]
     ))
   ];
--- user/code/lsp.nix
+++ user/code/lsp.nix
@@ -1,10 +1,12 @@
-{ pkgs, ... }:
-
-{
+{pkgs, ...}: {
   home.packages = with pkgs; [
     clang-tools
+    nixd
     lua-language-server
     cmake
+
+    bash-language-server
+    shellcheck
 
     stylua
     alejandra
