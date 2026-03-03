d21daedea15a57e082fdfb4aa66315ff6fbd646a
nicosobreira <nicolau.sobreira@gmail.com>
2026-03-03 12:31:16 -0300

    Declarative brave config

--- user/browser/brave.nix
+++ user/browser/brave.nix
@@ -1,5 +1,12 @@
 {pkgs, ...}: {
-  home.packages = with pkgs; [
-    brave
-  ];
+  programs.chromium = {
+    enable = true;
+    package = pkgs.brave;
+    extensions = [
+      {id = "eimadpbcbfnmbkopoojfekhnkhdbieeh";} # Dark Reader
+      {id = "ejkiikneibegknkgimmihdpcbcedgmpo";} # Volume Booster
+      {id = "khncfooichmfjbepaaaebmommgaepoid";} # Unhook
+      {id = "lloccabjgblebdmncjndmiibianflabo";} # Reading List
+    ];
+  };
 }
