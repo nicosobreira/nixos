e4e415edc77bf9c37c19c9cc36a04cf47936ada2
nicosobreira <nicolau.sobreira@gmail.com>
2026-03-08 15:18:06 -0300

    Add sega saturn emulator and remove nxengine

--- system/games/retro.nix
+++ system/games/retro.nix
@@ -5,11 +5,11 @@
     (retroarch.withCores (
       cores:
         with cores; [
-          nxengine
           snes9x
           pcsx2
           genesis-plus-gx
           beetle-psx-hw
+          beetle-saturn
           melonds
           mgba
         ]
