db52e72c20dedef5dcd78f09aeca76703255f8a9
nicosobreira <nicolau.sobreira@gmail.com>
2026-06-09 18:20:33 -0300

    Only one font config

--- system/fonts.nix
+++ system/fonts.nix
@@ -1,7 +1,11 @@
 {...}: {
   fonts.fontconfig = {
     antialias = true;
-    hinting.enable = true;
+    hinting = {
+      enable = true;
+      style = "slight"; # better for monospaced
+    };
+    subpixel.rgba = "rgb";
   };
 
   fonts.fontDir.enable = true;
--- user/shell/env.nix
+++ user/shell/env.nix
@@ -3,5 +3,7 @@
     EDITOR = userSettings.editor;
     TERMINAL = userSettings.terminal;
     BROWSER = userSettings.browser;
+
+    _JAVA_OPTIONS = "-Dawt.useSystemAAFontSettings=lcd -Dswing.aatext=true";
   };
 }
