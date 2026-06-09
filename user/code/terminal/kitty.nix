0542631b34ce0b4f6db3d5b1f2b8d73836a76393
nicosobreira <nicolau.sobreira@gmail.com>
2026-06-09 11:49:39 -0300

    Remove kitty config

--- user/code/terminal/kitty.nix
+++ user/code/terminal/kitty.nix
@@ -1,18 +1,6 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
+    kitty
     nerd-fonts.terminess-ttf
   ];
-
-  programs.kitty = {
-    enable = true;
-    settings = {
-      font_family = "Terminess Nerd Font Regular";
-      font_size = 12.0;
-
-      italic_font = "Terminess Nerd Font Regular";
-      bold_italic_font = "Terminess Nerd Font Bold";
-
-      text_composition_strategy = 1.0;
-    };
-  };
 }
