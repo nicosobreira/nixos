f8b9620e7e0a727d345c49e533d5e8438f19f08c
nicosobreira <nicolau.sobreira@gmail.com>
2026-06-09 21:59:47 -0300

    Add zen extensions

--- user/browser/zen.nix
+++ user/browser/zen.nix
@@ -1,9 +1,36 @@
-{pkgs, ...}: let
-  zen-browser = import (builtins.fetchTarball "https://github.com/youwen5/zen-browser-flake/archive/master.tar.gz") {
-    inherit pkgs;
+{
+  inputs,
+  pkgs,
+  lib,
+  ...
+}: let
+  extension = shortId: guid: {
+    name = guid;
+    value = {
+      install_url = "https://addons.mozilla.org/en-US/firefox/downloads/latest/${shortId}/latest.xpi";
+      installation_mode = "normal_installed";
+    };
   };
+
+  extensions = [
+    # To add additional extensions, find it on addons.mozilla.org, find
+    # the short ID in the url (like https://addons.mozilla.org/en-US/firefox/addon/!SHORT_ID!/)
+    # Then go to https://addons.mozilla.org/api/v5/addons/addon/!SHORT_ID!/ to get the guid
+    (extension "ublock-origin" "uBlock0@raymondhill.net")
+    (extension "competitive-companion" "{74e326aa-c645-4495-9287-b6febc5565a7}")
+    (extension "youtube-recommended-videos" "myallychou@gmail.com")
+  ];
 in {
   home.packages = [
-    zen-browser
+    (
+      pkgs.wrapFirefox
+      inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.zen-browser-unwrapped
+      {
+        extraPolicies = {
+          DisableTelemetry = true;
+          ExtensionSettings = builtins.listToAttrs extensions;
+        };
+      }
+    )
   ];
 }
