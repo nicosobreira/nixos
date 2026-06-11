22ca4181bce0ff2020f2aa3b219fbfccc9127b47
nicosobreira <nicolau.sobreira@gmail.com>
2026-06-11 15:53:07 -0300

    Revert "fix: environment variables"
    This reverts commit 1652fd417840b11648507d000fbba1c0cd145bcc.

--- home.nix
+++ home.nix
@@ -15,6 +15,14 @@
     ./user/browser/${userSettings.browser}.nix
   ];
 
+  home.sessionVariables = {
+    EDITOR = userSettings.editor;
+    TERMINAL = userSettings.terminal;
+    BROWSER = userSettings.browser;
+
+    _JAVA_OPTIONS = "-Dawt.useSystemAAFontSettings=lcd -Dswing.aatext=true";
+  };
+
   # Just don't change it
   home.stateVersion = "25.05";
 }
