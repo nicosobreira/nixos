909827ef3aa69cfac1826152c74acb2a1388f318
nicosobreira <nicolau.sobreira@gmail.com>
2026-06-14 12:42:32 -0300

    fix: using `pkgs.overrideAttrs` the right way

--- user/utils/nvidia-offload.nix
+++ user/utils/nvidia-offload.nix
@@ -5,21 +5,24 @@
 }: let
   isNvidiaOffloadEnabled = osConfig.hardware.nvidia.prime.offload.enable or false;
 
-  # Função que wrapa um pacote com nvidia-offload se NVIDIA estiver ativa,
-  # caso contrário retorna o pacote sem modificação
+  # If nvidia offload is enable then use it on `pkg` else do nothing
   withNvidiaOffload = pkg:
     if isNvidiaOffloadEnabled
     then
-      pkgs.runCommand "${pkg.name}-nvidia-offload"
-      {buildInputs = [pkgs.makeWrapper];}
-      ''
-        makeWrapper ${pkg}/bin/${pkg.meta.mainProgram or pkg.pname or pkg.name} \
-          $out/bin/${pkg.meta.mainProgram or pkg.pname or pkg.name} \
-          --set __NV_PRIME_RENDER_OFFLOAD 1 \
-          --set __NV_PRIME_RENDER_OFFLOAD_PROVIDER NVIDIA-G0 \
-          --set __GLX_VENDOR_LIBRARY_NAME nvidia \
-          --set __VK_LAYER_NV_optimus NVIDIA_only
-      ''
+      pkg.overrideAttrs (old: {
+        nativeBuildInputs = (old.nativeBuildInputs or []) ++ [pkgs.makeWrapper];
+        postInstall =
+          (old.postInstall or "")
+          + ''
+            for bin in $out/bin/*; do
+              wrapProgram "$bin" \
+                --set __NV_PRIME_RENDER_OFFLOAD 1 \
+                --set __NV_PRIME_RENDER_OFFLOAD_PROVIDER NVIDIA-G0 \
+                --set __GLX_VENDOR_LIBRARY_NAME nvidia \
+                --set __VK_LAYER_NV_optimus NVIDIA_only
+            done
+          '';
+      })
     else pkg;
 in {
   # Exporta a função para outros módulos via _module.args
