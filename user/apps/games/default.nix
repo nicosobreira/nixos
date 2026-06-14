8471f9182ffb932df39c1ef44011286e849244dd
nicosobreira <nicolau.sobreira@gmail.com>
2026-06-14 13:56:09 -0300

    ref: move lutris cofig to `lutris.nix` and use imports in `games`

--- user/apps/games/default.nix
+++ user/apps/games/default.nix
@@ -1,34 +1,7 @@
-{
-  pkgs,
-  withNvidiaOffload,
-  ...
-}: {
-  programs.lutris = {
-    enable = true;
-
-    package = withNvidiaOffload pkgs.lutris;
-
-    runners = {
-      pcsx2 = {
-        package = pkgs.pcsx2;
-        settings = {
-          runner.fullscreen = true;
-        };
-      };
-    };
-  };
-
-  home.packages = with pkgs; [
-    antimicrox
-
-    # Na hora da instalação, o $HOME é sobre escrito, por isso tem que fazer isso
-    # (withNvidiaOffload (pkgs.dolphin-emu-primehack.overrideAttrs (old: {
-    #   postInstall =
-    #     (old.postInstall or "")
-    #     + ''
-    #       wrapProgram $out/bin/primehack \
-    #         --run 'export HOME=$(getent passwd $USER | cut -d: -f6)'
-    #     '';
-    # })))
+{...}: {
+  imports = [
+    ./retro.nix
+    ./lutris.nix
+    ./minecraft.nix
   ];
 }
---
+++ user/apps/games/lutris.nix
@@ -0,0 +1,24 @@
+{
+  pkgs,
+  withNvidiaOffload,
+  ...
+}: {
+  programs.lutris = {
+    enable = true;
+
+    package = withNvidiaOffload pkgs.lutris;
+
+    runners = {
+      pcsx2 = {
+        package = pkgs.pcsx2;
+        settings = {
+          runner.fullscreen = true;
+        };
+      };
+    };
+  };
+
+  home.packages = with pkgs; [
+    antimicrox
+  ];
+}
