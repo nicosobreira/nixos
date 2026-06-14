37b2d9ebf8d85eb65a2aedbbbccc0ba0fc18cc2f
nicosobreira <nicolau.sobreira@gmail.com>
2026-06-14 17:59:20 -0300

    add: things to happen when the lid is close

--- system/hardware/power.nix
+++ system/hardware/power.nix
@@ -14,4 +14,11 @@
       energy_performance_preference = "balance_performance";
     };
   };
+
+  # Um entre "ignore", "poweroff", "reboot", "halt", "kexec", "suspend", "hibernate", "hybrid-sleep", "suspend-then-hibernate", "lock"
+  services.logind.settings.Login = {
+    HandleLidSwitch = "lock"; # Caso esteja na bateria
+    HandleLidSwitchExternalPower = "lock"; # Caso esteja na tomada
+    HandleLidSwitchDocked = "ignore"; # Caso outra tela esteja conectada
+  };
 }
