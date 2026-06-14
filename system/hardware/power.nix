be81fcdfd3f1bddc9dca2c1b88e87b432345e859
nicosobreira <nicolau.sobreira@gmail.com>
2026-06-14 17:59:01 -0300

    fix: auto-cpufreq better options and conflit with powerManagment

--- system/hardware/power.nix
+++ system/hardware/power.nix
@@ -1,15 +1,17 @@
 {...}: {
-  powerManagement.enable = true;
+  powerManagement.enable = false;
 
   services.auto-cpufreq.enable = true;
   services.auto-cpufreq.settings = {
     battery = {
       governor = "powersave";
       turbo = "never";
+      energy_performance_preference = "power";
     };
     charger = {
-      governor = "schedutil";
+      governor = "powersave";
       turbo = "auto";
+      energy_performance_preference = "balance_performance";
     };
   };
 }
